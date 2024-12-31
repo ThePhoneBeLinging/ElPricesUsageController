026628fa8905c19fa597f35b2e6c5dd1fb2b5eb9
EAL <elias@takefive.dk>
2024-12-31 13:43:02 +0100

    db_ is now initialised in the constructor

--- src/PulseStorage.cpp
+++ src/PulseStorage.cpp
@@ -6,10 +6,10 @@
 
 #include <iostream>
 
-PulseStorage::PulseStorage() : keepRunning_(true),memoryDB_(std::make_unique<SQLite::Database>(":memory:",SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE))
+PulseStorage::PulseStorage() : db_(std::make_unique<SQLite::Database>("../../HistoricData/Pulses.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE))
+, keepRunning_(true),memoryDB_(std::make_unique<SQLite::Database>(":memory:",SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE))
 {
     std::cout << "PulseStorage constructor" << std::endl;
-    db_ = std::make_unique<SQLite::Database>("../../HistoricData/Pulses.db", SQLite::OPEN_READWRITE|SQLite::OPEN_CREATE);
     // This part of the constructor creates a Table with the same specifications of the file-based DB
     std::string query = "CREATE TABLE Pulses ("
     "\"ID\" INTEGER PRIMARY KEY AUTOINCREMENT,"
